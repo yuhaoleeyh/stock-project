f2ce1583071c3022399db42f3e7037e0edad4240
yuhaolee97 <yuhaolee97@gmail.com>
2020-10-18 14:09:49 +0800

    Cleaning up of code + adding of model result diagram

--- data.py
+++ data.py
@@ -6,79 +6,40 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import numpy as np
  
-# import keras
-# import tensorflow as tf
-# from keras.models import Model
-# from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
-# from keras import optimizers
- 
  
 start_date = datetime(2005, 9, 1)
 end_date = datetime(2020, 8, 31)
  
-# stocks = ['INPX', 'AAPL', 'MSFT', '^GSPC']
  
 dataset = []
 test = []
  
-panel_data = data.DataReader('MSFT', start = start_date, end = end_date, data_source = 'yahoo')
-panel_data.to_csv('./microsoft_stocks_data.csv')
-# google = pd.read_csv('./google_stocks_data.csv')
-# apple = pd.read_csv('./apple_stocks_data.csv')
-microsoft = pd.read_csv('./microsoft_stocks_data.csv')
-# gspc = pd.read_csv('./gspc_stocks_data.csv')
-#
+
+
+#pulling of google data from csv file
+google = pd.read_csv('D:/didi/ml/stock-project/google_stocks_data.csv')
+
  
 history_points = 50
+
  
-# print(panel_data)
-# dataset.append(panel_data.assign(i = i)[['Adj Close']])
-    # print(panel_data.shape)
-    # print(panel_data)
-    # print(stocks[i])
- 
-# panel_data.to_csv('./stocks_data.csv')
- 
-stocks = microsoft
- 
-# for i in range(len(stocks)):
-#     print(stocks[i].head())
+stocks = google
+
  
  
 stocks.drop(columns = ['Date'], inplace = True, axis = 1)
-# print(stocks.head())
- 
-# for i in range(len(stocks)):
-#     print(stocks[i].shape)
- 
-# for i in range(len(stocks)):
-#     print(stocks[i].describe())
- 
-# for i in range(len(stocks)):
-#     lists = [0]
-#     for j in range(1, len(stocks[i]['Adj Close'])):
-#         lists.append((stocks[i]['Adj Close'][j] - stocks[i]['Adj Close'][j - 1]) * 100.0 / stocks[i]['Adj Close'][j - 1]) 
-#     stocks[i]['Percentage change'] = lists
-#     print(stocks[i][stocks[i]['Percentage change'] > 15])
- 
- 
-# print(stocks.shape[0])
  
 test_split = 0.7 # the percent of data to be used for testing
 n = int(stocks.shape[0] * test_split)
  
 train_data = stocks[:n]
 
-print(train_data)
  
+#normalise data so that various columns will have equal weight
 normaliser = preprocessing.MinMaxScaler()
 normalised_data = normaliser.fit_transform(train_data)
- 
-    # for j in range(5):
-    #     print(normalised_data[j])
-# print(normalised_data[:,0])
-# print(stocks['High'][0:4])
- 
+
+#preparing of input and output
 ohlcv_histories_normalised = np.array([normalised_data[i  : i + history_points].copy() for i in range(len(normalised_data) - history_points)])
 next_day_open_values_normalised = np.array([normalised_data[:,2][i + history_points].copy() for i in range(len(normalised_data) - history_points)])
 next_day_open_values_normalised = np.expand_dims(next_day_open_values_normalised, -1)
@@ -89,21 +50,17 @@ next_day_open_values = np.array([train_data['Open'][i + history_points].copy() f
  
 next_day_open_values = np.expand_dims(next_day_open_values, -1)
  
- 
- 
-# print(next_day_open_values)
-# print(normalised_data[:,2])
- 
+
+#normalise y so we can do an inverse later on once we test the result
 y_normaliser = preprocessing.MinMaxScaler()
 y_normaliser.fit(next_day_open_values)
  
  
 assert ohlcv_histories_normalised.shape[0] == next_day_open_values_normalised.shape[0]
  
+#same thing is done for test data. note test and train data must always be separate from one another, ie. the model should never have the test set during the training phase 
 test_data = stocks[n:]
- 
-# print(test_data)
- 
+  
  
 test_normalised_data = normaliser.transform(test_data)
  
@@ -112,10 +69,7 @@ next_day_open_values_normalised_test = np.array([test_normalised_data[:,2][i + h
 next_day_open_values_normalised_test = np.expand_dims(next_day_open_values_normalised_test, -1)
  
  
-# print(len(test_data) - history_points)
- 
- 
-next_day_open_values_test = np.array([test_data['Open'][2643 + i + history_points].copy() for i in range(len(test_data) - history_points)])
+next_day_open_values_test = np.array([test_data['Open'][1762 + i + history_points].copy() for i in range(len(test_data) - history_points)])
  
 next_day_open_values_test = np.expand_dims(next_day_open_values_test, -1)
  
@@ -130,8 +84,8 @@ y_test = next_day_open_values_normalised_test
 unscaled_y_test = next_day_open_values_test
  
 number = int(0.9 * len(train_data))
-# print(train_data[:number])
-# print(len(ohlcv_train))
+
+#prepare second feature, exponential moving average
  
 exponential_moving_average = [] #factors in more weight to recent data rather than a simple moving average which weighs everything equally (not good for stock data )
  
@@ -150,7 +104,7 @@ exponential_moving_average_normalised = scaler.fit_transform(exponential_moving_
  
 assert exponential_moving_average_normalised.shape[0] == y_train.shape[0]
  
- 
+ #second feature for test data
 exponential_moving_test = []
  
  
@@ -159,70 +113,15 @@ sma = np.mean(ohlcv_test[0][:,3])
 exponential_moving_test.append(np.array([sma]))
  
 for i in range(1, len(ohlcv_test)):
-    sma = (test_data['Close'][i + 2643] - sma) * multiplier + sma
+    sma = (test_data['Close'][i + 1762] - sma) * multiplier + sma
     exponential_moving_test.append(np.array([sma]))
  
 exponential_moving_test_normalised = scaler.transform(exponential_moving_test)
  
 assert exponential_moving_test_normalised.shape[0] == y_test.shape[0]
+
  
-# print(exponential_moving_test_normalised)
- 
-# # print(ohlcv_histories_normalised.shape[0])
-# # print(ohlcv_histories_normalised.shape[1])
-# # print(ohlcv_histories_normalised.shape[2])
-# # print(ohlcv_histories_normalised.shape[3])
-# # print(ohlcv_histories_normalised.shape[4])
- 
- 
-y_normaliser = preprocessing.MinMaxScaler()
-y_normaliser.fit(next_day_open_values)
- 
-# print(exponential_moving_average_normalised.shape[1])
- 
-# def simple_model():
-#     import keras
-#     import tensorflow as tf
-#     from keras.models import Model
-#     from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
-#     from keras import optimizers
-#     from keras.callbacks import History 
-#     import numpy as np
-#     np.random.seed(4)
-#     tf.random.set_seed(4)
- 
-#     # history = History()
- 
- 
-#     lstm_input = Input(shape=(history_points, 6), name='lstm_input')
-#     x = LSTM(50, name='lstm_0')(lstm_input)
-#     x = Dropout(0.2, name='lstm_dropout_0')(x)
-#     x = Dense(64, name='dense_0')(x)
-#     x = Activation('sigmoid', name='sigmoid_0')(x)
-#     x = Dense(1, name='dense_1')(x)
-#     # x = Dropout(0.2, name='lstm_dropout_1')(x)
-#     output = Activation('linear', name='linear_output')(x)
-#     model = Model(inputs=lstm_input, outputs=output)
- 
-#     adam = optimizers.Adam(lr=0.0005)
- 
-#     model.compile(optimizer=adam, loss='mse')
- 
-#     return model
- 
-    # from keras.utils.vis_utils import plot_model
-    # plot_model(model, to_file='model.png')
-# from sklearn.model_selection import cross_val_score
-# from sklearn.datasets import load_iris
-# from sklearn.ensemble import AdaBoostClassifier
-# from keras.wrappers.scikit_learn import KerasRegressor
-# from sklearn.ensemble import AdaBoostRegressor
- 
-# # estimator = KerasRegressor(build_fn = simple_model, epochs = 50, batch_size = 32, verbose = False)
- 
-# # boosted_result = AdaBoostRegressor(base_estimator = estimator)
-# # boosted_result.fit(ohlcv_train, y_train)
- 
+#machine learning libraries to import 
 import keras
 import tensorflow as tf
 from keras.models import Model
@@ -233,24 +132,12 @@ import numpy as np
 np.random.seed(4)
 tf.random.set_seed(4)
  
-# history = History()
  
- 
+#prepare the 2 features to be inputted into model 
 lstm_input = Input(shape=(history_points, 6), name='lstm_input')
 exponential_input = Input(shape=(exponential_moving_average_normalised.shape[1],), name='ema_input')
  
-# x = LSTM(50, name='lstm_0')(lstm_input)
-# x = Dense(70, name='dense_0')(x)
-# # x = Dropout(0.2, name='lstm_dropout_0')(x)
-# x = Activation('sigmoid', name='relu_1')(x)
-# x = Dense(1, name='dense_2')(x)
- 
-# # x = Dropout(0.2, name='lstm_dropout_0')(x)
-# # x = Dropout(0.2, name='lstm_dropout_1')(x)
-# output = Activation('linear', name='linear_output')(x)
-# model = Model(inputs=lstm_input, outputs=output)
- 
-# the first branch operates on the first input
+#first branch
 x = LSTM(50, name='lstm_0')(lstm_input)
 x = Dropout(0.2, name='dropout_0')(x)
 lstm_branch = Model(inputs=lstm_input, outputs=x)
@@ -275,52 +162,14 @@ model = Model(inputs=[lstm_branch.input, average_branch.input], outputs=z)
 adam = optimizers.Adam(lr=0.0005)
  
 model.compile(optimizer=adam, loss='mse')
- 
+#fitting of model. note the validation split of 0.1 
 model.fit(x=[ohlcv_train, exponential_moving_average_normalised], y=y_train, batch_size=32, epochs=100, shuffle=True, validation_split = 0.1) #callbacks = [history]
  
-# from keras.utils import plot_model
-# plot_model(model, to_file='model.png', show_shapes=True)
- 
-#can pass in the validation state (validation_set)
- 
-#validation error
-#evaluation metric (validation mse, train mse) #keras use plot history. 
-# plot performance. every iteration has an mse value. to see whats happening.
-#more layers. performance matric. 
- 
-#each iteration 200 jobs. train mse. run on the validation data. signs of overfitting on underfitting. trained mse goes down validation mse goes up 
-#boosting LSTM article (BOOSTING, residual -> ) (boosting I )
-# hmm -> HMM apply -> documentation. train HMM resources.  
-#   
- 
-# model.save("my_model")
- 
-# model = keras.models.load_model('./my_model')
- 
-# losses = history.history
- 
-# evaluation = model.evaluate(ohlcv_train, y_train)
-# print(evaluation)
-# print(losses)
- 
-# plt.plot(losses['loss'][0:-1], label = 'Training Loss')
-# plt.plot(losses['val_loss'][0:-1], label = 'Validation Loss')
- 
-# plt.legend(['Training Loss', 'Validation Loss'])
- 
-# plt.show()
- 
-# history.history.to_csv('./modelhistory.csv')
- 
-# histories = pd.read_csv('./modelhistory.csv')
- 
-# print(histories)
- 
+
+#predicting of model against test 
 y_test_predicted = model.predict([ohlcv_test, exponential_moving_test_normalised])
 y_test_predicted = y_normaliser.inverse_transform(y_test_predicted)
-# y_predicted = model.predict(ohlcv_histories_normalised)
-# y_predicted = y_normaliser.inverse_transform(y_predicted)
- 
+
 assert unscaled_y_test.shape == y_test_predicted.shape
 real_mse = np.mean(np.square(unscaled_y_test - y_test_predicted))
 scaled_mse = real_mse / (np.max(unscaled_y_test) - np.min(unscaled_y_test)) * 100
@@ -334,55 +183,10 @@ end = -1
 real = plt.plot(unscaled_y_test[start:end], label='real')
 pred = plt.plot(y_test_predicted[start:end], label='predicted')
  
-# real = plt.plot(unscaled_y[start:end], label='real')
-# pred = plt.plot(y_predicted[start:end], label='predicted')
- 
+
+#plotting of model  
 plt.legend(['Real', 'Predicted'])
  
 plt.show()
 
 
-
-
-
-
-# for i in range(len(stocks)):
-#     print(stocks[i].isnull().sum().sum())
-#no severe abnomalies. good! 
-
-# for i in range(len(stocks)):
-    # print(stocks[i][stocks[i]['High'] > stocks[i]['Open']])
-    #check the high is higher than the open and close, and low is lower then the open and close
-
-# for i in range(len(stocks)):
-#     for j in range(0, 1000):
-#         if j % 20 == 0:
-#             print(j)
-#             rand_number = randint(10, 20) #between 10-20 days of data
-            
-
-
-
-
-
-
-# dataset.append(['Adj Close'])
-# print(panel_data.describe())
-# test.append(panel_data)
-
-# print(panel_data.isnull().sum().sum()) #check there is no null value inside. 
-# print(test)
-# test_stocks = pd.concat(test, axis = 1)
-
-# df_stocks = pd.concat(panel_data, axis = 1)
-# display = df_stocks.head(5)
-
-
-
-# plt.figure(figsize=(16,8))
-# plt.plot(df_stocks, label = "Adjusted close prize history")
-# plt.show()
-
-
-
-# print(eda(df_stocks))
